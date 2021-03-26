6bd4fb3411f94ecf05b48171ec118fa21efa8a5a
Manuel Jesus Jaen Lopez <manuel.jaen.lopez@gmail.com>
2021-03-26 09:49:33 -0500

    Data structures

---
+++ datastructure/src/test/java/org/mjjaen/datastructure/DataStructureTest.java
@@ -0,0 +1,15 @@
+package org.mjjaen.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class DataStructureTest {
+    @Test
+    public void test() {
+
+    }
+}
