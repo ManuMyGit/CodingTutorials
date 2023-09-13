b22b1181eb51a1d6ad8beebd17824275c3b789c3
Manuel Jesus Jaen Lopez <manuel.jaen.lopez@gmail.com>
2023-09-13 22:13:40 +0100

    Graph data structure

--- java/misc/datastructure/src/main/java/org/mjjaen/datastructure/graphs/examples/JGraphTExample.java
+++ java/misc/datastructure/src/main/java/org/mjjaen/datastructure/graphs/examples/JGraphTExample.java
@@ -2,9 +2,11 @@ package org.mjjaen.datastructure.graphs.examples;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultDirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.alg.shortestpath.BellmanFordShortestPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.*;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -20,14 +22,77 @@ public class JGraphTExample extends GraphExample {
 
     @Override
     public void run() {
+        testSimpleGraph();
+
+        testDirectedGraphWithComplexObjectAsVertex();
+
+        testGrapthWithLabeledEdge();
+
+        testIterators();
+    }
+
+    void testSimpleGraph() {
         Graph<String, DefaultEdge> stringGraph = createStringGraph();
         log.info(stringGraph.toString());
+    }
 
+    private Graph<String, DefaultEdge> createStringGraph() {
+        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+        String v4 = "v4";
+
+        // add the vertices
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addVertex(v4);
+
+        // add edges to create a circuit
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3);
+        g.addEdge(v3, v4);
+        g.addEdge(v4, v1);
+
+        return g;
+    }
+
+    void testDirectedGraphWithComplexObjectAsVertex() {
         Graph<URI, DefaultEdge> hrefGraph = createHrefGraph();
         log.info(hrefGraph.toString());
         URI start = hrefGraph.vertexSet().stream().filter(uri -> uri.getHost().equals("www.jgrapht.org")).findAny().get();
         log.info(start.toString());
+    }
 
+    private Graph<URI, DefaultEdge> createHrefGraph() {
+        Graph<URI, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+        URI google = null, wikipedia = null, jgrapht = null;
+        try {
+            google = new URI("http://www.google.com");
+            wikipedia = new URI("http://www.wikipedia.org");
+            jgrapht = new URI("http://www.jgrapht.org");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        // add the vertices
+        g.addVertex(google);
+        g.addVertex(wikipedia);
+        g.addVertex(jgrapht);
+
+        // add edges to create linking structure
+        g.addEdge(jgrapht, wikipedia);
+        g.addEdge(google, jgrapht);
+        g.addEdge(google, wikipedia);
+        g.addEdge(wikipedia, google);
+
+        return g;
+    }
+
+    void testGrapthWithLabeledEdge() {
         Graph<String, RelationshipEdge> graph = new DefaultDirectedGraph<>(RelationshipEdge.class);
         List<String> people = new ArrayList<>();
         people.add("John");
@@ -56,53 +121,73 @@ public class JGraphTExample extends GraphExample {
         assert graph.getEdge("James", "John").getLabel().equals(ENEMY);
     }
 
-    private Graph<String, DefaultEdge> createStringGraph() {
-        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+    void testIterators() {
+        Graph<String, DefaultWeightedEdge> g = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 
-        String v1 = "v1";
-        String v2 = "v2";
-        String v3 = "v3";
-        String v4 = "v4";
+        String vA = "A";
+        String vB = "B";
+        String vC = "C";
+        String vD = "D";
+        String vE = "E";
+        String vF = "F";
+        String vG = "G";
+        String vH = "H";
 
         // add the vertices
-        g.addVertex(v1);
-        g.addVertex(v2);
-        g.addVertex(v3);
-        g.addVertex(v4);
+        g.addVertex(vA);
+        g.addVertex(vB);
+        g.addVertex(vC);
+        g.addVertex(vD);
+        g.addVertex(vE);
+        g.addVertex(vF);
+        g.addVertex(vG);
+        g.addVertex(vH);
 
         // add edges to create a circuit
-        g.addEdge(v1, v2);
-        g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v1);
+        DefaultWeightedEdge e1 = g.addEdge(vA, vB);
+        g.setEdgeWeight(e1, 5);
 
-        return g;
-    }
+        DefaultWeightedEdge e2 = g.addEdge(vB, vC);
+        g.setEdgeWeight(e2, 100);
 
-    private Graph<URI, DefaultEdge> createHrefGraph() {
-        Graph<URI, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+        DefaultWeightedEdge e3 = g.addEdge(vB, vH);
+        g.setEdgeWeight(e3, 5);
 
-        URI google = null, wikipedia = null, jgrapht = null;
-        try {
-            google = new URI("http://www.google.com");
-            wikipedia = new URI("http://www.wikipedia.org");
-            jgrapht = new URI("http://www.jgrapht.org");
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+        DefaultWeightedEdge e4 = g.addEdge(vC, vE);
+        g.setEdgeWeight(e4, 5);
+
+        DefaultWeightedEdge e5 = g.addEdge(vC, vD);
+        g.setEdgeWeight(e5, 5);
+
+        DefaultWeightedEdge e6 = g.addEdge(vH, vE);
+        g.setEdgeWeight(e6, 5);
+
+        DefaultWeightedEdge e7 = g.addEdge(vE, vF);
+        g.setEdgeWeight(e7, 5);
+
+        DefaultWeightedEdge e8 = g.addEdge(vE, vG);
+        g.setEdgeWeight(e8, 5);
+
+
+        DepthFirstIterator<String, DefaultWeightedEdge> depthFirstIterator = new DepthFirstIterator<>(g);
+        while(depthFirstIterator.hasNext()) {
+            String aux = depthFirstIterator.next();
+            log.info(aux);
         }
 
-        // add the vertices
-        g.addVertex(google);
-        g.addVertex(wikipedia);
-        g.addVertex(jgrapht);
+        BreadthFirstIterator<String, DefaultWeightedEdge> breadthFirstIterator = new BreadthFirstIterator<>(g);
+        while(breadthFirstIterator.hasNext()) {
+            String aux = breadthFirstIterator.next();
+            log.info(aux);
+        }
 
-        // add edges to create linking structure
-        g.addEdge(jgrapht, wikipedia);
-        g.addEdge(google, jgrapht);
-        g.addEdge(google, wikipedia);
-        g.addEdge(wikipedia, google);
+        DijkstraShortestPath<String, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(g);
+        List<String> shortestPath = dijkstraShortestPath.getPath(vA,vD).getVertexList();
+        log.info(shortestPath.toString());
 
-        return g;
+        BellmanFordShortestPath<String, DefaultWeightedEdge> bellmanFordShortestPath = new BellmanFordShortestPath<>(g);
+        List<String> shortestPath2 = bellmanFordShortestPath.getPath(vA,vD).getVertexList();
+        log.info(shortestPath2.toString());
     }
 }
 
